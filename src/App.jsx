f8bcdddfa48e3f6ede8bbc4ba9ec39a2b67fe1f7
Simranjeet Singh <simranjeets224@gmail.com>
2022-10-29 23:54:00 -0700

    Testing branch connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import TeamTable from "./components/TeamStandingtable";
 import DriverTable from "./components/DriverTable";
 import NavBar from "./components/NavBar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 function App() {
   return (
     <div className="w-full ">
